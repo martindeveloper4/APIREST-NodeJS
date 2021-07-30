b162e74e021ef7e15ab2a99718f255276e47404b
Martin Luque <martindeveloper4@gmail.com>
2021-07-29 22:17:49 -0500

    node

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 4000;
 
 // servir contenido estatico {Middlewares}
-/*
-app.use( express.static('public'));
-*/
 
+app.use( express.static('public'));
+
+/*
 app.get('/',function(req,res){
     res.send('Home Page');
 });
-
+*/
 
 app.get('/hola-mundo',function(req,res){
     res.send('Hola mundo en su respectiva ruta');
